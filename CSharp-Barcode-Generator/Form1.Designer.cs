b985ac95793aeda9244c4a1efb2251e0b252bea4
akshaymu <akshaymu@ICS-LT-66Q1ZD3>
2024-01-25 19:30:07 +0530

    Add project files.

---
+++ CSharp-Barcode-Generator/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace CSharp_Barcode_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BarcodeBox = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.BarcodeBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BarcodeBox
+            // 
+            this.BarcodeBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BarcodeBox.Location = new System.Drawing.Point(82, 44);
+            this.BarcodeBox.Name = "BarcodeBox";
+            this.BarcodeBox.Size = new System.Drawing.Size(593, 216);
+            this.BarcodeBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BarcodeBox.TabIndex = 0;
+            this.BarcodeBox.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(145)))), ((int)(((byte)(141)))));
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(82, 280);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(593, 29);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(145)))), ((int)(((byte)(141)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(165, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(145)))), ((int)(((byte)(141)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(431, 325);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 43);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Download";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.White;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Cascadia Mono", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Gray;
+            this.linkLabel1.Location = new System.Drawing.Point(257, 412);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(232, 17);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Click here for more projects";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(233, 385);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 27);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Created by AKSHAY MUDDA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(758, 449);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.BarcodeBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "C# Barcode Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.BarcodeBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox BarcodeBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CSharp-Barcode-Generator/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using ZXing;
+
+namespace CSharp_Barcode_Generator
+{
+    public partial class Form1 : Form
+    {
+        private int maxCharacterLimit = 50; // Set your desired character limit here
+        private string defaultWatermarkText = "Bar code Data (Click here)";
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Set the default watermark text
+            SetWatermark();
+        }
+        private void SetWatermark()
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                textBox1.Text = defaultWatermarkText;
+                textBox1.ForeColor = SystemColors.WindowText;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GenerateBarcode();
+        }
+
+        private void GenerateBarcode()
+        {
+            string inputValue = textBox1.Text.Trim();
+
+            if (!string.IsNullOrEmpty(inputValue) && inputValue != defaultWatermarkText)
+            {
+                if (inputValue.Length <= maxCharacterLimit)
+                {
+                    BarcodeWriter barcodeWriter = new BarcodeWriter();
+                    barcodeWriter.Format = BarcodeFormat.CODE_128;
+
+                    Bitmap barcodeBitmap = new Bitmap(barcodeWriter.Write(inputValue));
+
+                    if (barcodeBitmap.Width > BarcodeBox.Width)
+                    {
+                        // Calculate scaling factor to fit within PictureBox width
+                        float scale = (float)BarcodeBox.Width / barcodeBitmap.Width;
+
+                        // Create a new bitmap with scaled dimensions
+                        Bitmap scaledBarcodeBitmap = new Bitmap(barcodeBitmap, new Size((int)(barcodeBitmap.Width * scale), (int)(barcodeBitmap.Height * scale)));
+
+                        // Set the scaled image to PictureBox
+                        BarcodeBox.Image = scaledBarcodeBitmap;
+                    }
+                    else
+                    {
+                        // Set the original image to PictureBox
+                        BarcodeBox.Image = barcodeBitmap;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"Please enter a value with at most {maxCharacterLimit} characters.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a value in the Barcode Box before generating a barcode.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (BarcodeBox.Image != null)
+            {
+                Bitmap bitmap = new Bitmap(BarcodeBox.Image);
+
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "PNG File (*.png)|*.png|Bitmap (*.bmp)|*.bmp";
+                saveFileDialog.Title = "Save Bar Code";
+                saveFileDialog.FileName = "Bar_Code";
+
+                if (saveFileDialog.ShowDialog()== DialogResult.OK)
+                {
+                    bitmap.Save(saveFileDialog.FileName);
+                    MessageBox.Show("Image saved to your computer","Image Saved", MessageBoxButtons.OK);
+                }
+                bitmap.Dispose();
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Check if the pressed key is Enter
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                // Trigger the same functionality as the "Generate" button click
+                GenerateBarcode();
+                e.Handled = true; // Handle the key event to prevent it from being processed further
+            }
+            else if (e.KeyChar != (char)Keys.Back)
+            {
+                // Check if the entered character is a valid character (alphabet, number, space, hyphen, or underscore)
+                bool isValidCharacter = char.IsLetterOrDigit(e.KeyChar) || e.KeyChar == ' ' || e.KeyChar == '-' || e.KeyChar == '_';
+
+                // If not a valid character, handle the key event to prevent it from being processed further
+                if (!isValidCharacter)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string githubProfileLink = "https://github.com/akshay-mudda";
+
+            // Open the link in the default web browser
+            Process.Start(githubProfileLink);
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            // When the TextBox gets focus, remove the watermark text if it's the default watermark
+            if (textBox1.Text == defaultWatermarkText)
+            {
+                textBox1.Text = string.Empty;
+                textBox1.ForeColor = SystemColors.WindowText;
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            // When the TextBox loses focus and is empty, display the watermark text
+            SetWatermark();
+        }
+    }
+}
